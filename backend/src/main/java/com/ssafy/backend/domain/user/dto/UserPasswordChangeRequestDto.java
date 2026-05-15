9be87ddb6b5ce495a93113376c9077f7b1b099d6
나예지 <nej0720@naver.com>
2026-05-15 13:33:27 +0900

    fix: 비밀번호 허용 특수문자 정책 조정

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/UserPasswordChangeRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/UserPasswordChangeRequestDto.java
@@ -11,8 +11,7 @@ public record UserPasswordChangeRequestDto(
     @NotBlank(message = "새 비밀번호는 필수입니다.")
         @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해야 합니다.")
         @Pattern(
-            regexp =
-                "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;,.?/~])[A-Za-z\\d!@#$%^&*()_+\\-={}\\[\\]:;,.?/~]+$",
-            message = "비밀번호는 영문, 숫자, 특수문자를 각각 1자 이상 포함해야 합니다.")
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]+$",
+            message = "비밀번호는 영문, 숫자, 특수문자(!@#$%^&*)를 각각 1자 이상 포함해야 합니다.")
         @Schema(description = "새 비밀번호", example = "NewPassword1!")
         String newPassword) {}
